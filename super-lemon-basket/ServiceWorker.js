808dd01d361c2a6340f8c42c86f09ad908b957d1
BioticGrease94 <emannuelrdzesp150713@gmail.com>
2026-04-23 00:16:19 +0000

    1

---
+++ super-lemon-basket/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "SamLeigh-Lemon Basket-1.0";
+const contentToCache = [
+    "/Build/Cool Math Build.loader.js",
+    "/Build/Cool Math Build.framework.js.br",
+    "/Build/Cool Math Build.data.br",
+    "/Build/Cool Math Build.wasm.br",
+    "/TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
